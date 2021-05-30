07924a809dbe21c05032ba7d92730877ca1e52a6
bfayers <bfayers@users.noreply.github.com>
2021-05-30 19:14:12 +0100

    Create LiveSession class and move pause/unpause functions there

--- eocharging/ChargingData.py
+++ eocharging/ChargingData.py
@@ -3,8 +3,8 @@ import requests
 
 
 class Session:
+    """Class that defines what a Session is"""
     def __init__(self, access_token=None, cpid=None, start_date=None, end_date=None):
-        """Nothing yet"""
         if access_token is None:
             raise Exception("No access_token provided")
         if cpid is None:
@@ -40,3 +40,36 @@ class Session:
             kwh = ((point["CT2"] / 1000) * 230) / 1000 / 60
             self.session_kwh += kwh
             self.session_cost += kwh * point["Cost"]
+
+class LiveSession:
+    def __init__(self, access_token=None):
+        if access_token is None:
+            raise Exception("No access_token provided")
+        
+        url = base_url + "api/session"
+        headers = {"Authorization": "Bearer " + access_token}
+
+        response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            raise Exception("Response was not OK")
+        
+        #I don't know the structure of this data until I next charge a car; so this is TODO
+        data = response.json()
+    
+    def pause(self):
+        """Used for pausing a session
+        Similar to disabling/locking but also not"""
+        url = base_url + "api/session/pause"
+        payload = {"id": self.device_address}
+        response = requests.post(url, data=payload, headers=self.headers)
+        if response.status_code != 200:
+            raise Exception("Response was not OK")
+
+    def unpause(self):
+        """Used for unpausing a paused session
+        Similar to enabling/unlocking but also not"""
+        url = base_url + "api/session/unpause"
+        payload = {"id": self.device_address}
+        response = requests.post(url, data=payload, headers=self.headers)
+        if response.status_code != 200:
+            raise Exception("Response was not OK")
