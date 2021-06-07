987750c561df128aa7d193ff6e49a5195d79206d
bfayers <bfayers@users.noreply.github.com>
2021-06-07 21:36:58 +0100

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="pyEOCharging",
     packages=find_packages(include=["eocharging"]),
-    version="0.0.3",
+    version="0.0.4",
     description="EO Smart Charger Library",
     long_description=long_description,
     long_description_content_type="text/markdown",
